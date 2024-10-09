782b6e1f6bfcc350cea2d59de4bdfac596e1910e
tolfromj <wlgusehd0@gmail.com>
2024-10-09 14:10:07 +0000

    fixed untracked files

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 
 import torch
-from transformers import DetrConfig, DetrForObjectDetection
+from transformers import DetrConfig, DetrForObjectDetection, Detr
 from ultralytics import YOLO
 
 
@@ -19,7 +19,14 @@ class HuggingfaceModel:
             following 2 keys: 'class_labels' and 'boxes' (the class labels and bounding boxes of an image in the batch
             respectively). The class labels themselves should be a `torch.LongTensor` of len `(number of bounding boxes
             in the image,)` and the boxes a `torch.FloatTensor` of shape `(number of bounding boxes in the image, 4)`.
-
+        
+        DETR resizes the input images such that the shortest side is at least a certain amount of pixels while the longest 
+        is at most 1333 pixels. At training time, scale augmentation is used such that the shortest side is randomly set to 
+        at least 480 and at most 800 pixels. At inference time, the shortest side is set to 800. One can use 
+        DetrImageProcessor to prepare images (and optional annotations in COCO format) for the model. Due to this resizing, 
+        images in a batch can have different sizes. DETR solves this by padding images up to the largest size in a batch, 
+        and by creating a pixel mask that indicates which pixels are real/which are padding. Alternatively, one can also 
+        define a custom collate_fn in order to batch images together, using ~transformers.DetrImageProcessor.pad_and_create_pixel_mask.
         """
         labels = {}
         labels["boxes"] = targets["boxes"]
